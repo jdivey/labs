36de20a40a239034f5bafb1f598d4cf4a420d84d
jdivey <jdivey23@gmail.com>
2019-12-03 12:36:59 -0500

    New commit Date: 12-3-19 Time: 12:36 pm.

--- final_project/views/vehicle/edit/vehicle_edit.class.php
+++ final_project/views/vehicle/edit/vehicle_edit.class.php
@@ -16,6 +16,7 @@ class VehicleEdit extends VehicleIndexView
         //retrieve movie details by calling get methods
         $id = $vehicle->getId();
         $model = $vehicle->getModel();
+        $year = $vehicle->getYear();
         $price = $vehicle->getPrice();
         $stock = $vehicle->getStock();
         $image = $vehicle->getImage();
@@ -29,14 +30,15 @@ class VehicleEdit extends VehicleIndexView
             <input type="hidden" name="id" value="<?= $id ?>">
             <p><strong>Model</strong><br>
                 <input name="model" type="text" size="100" value="<?= $model ?>" required autofocus></p>
-            <p><strong>Price</strong>: <input name="price" type="date" value="<?= $price ?>" required=""></p>
-            <p><strong>Stock</strong>: Separate directors with commas<br>
+            <p><strong>Year</strong>: <input name="year" type="text" value="<?= $year ?>" required=""></p>
+            <p><strong>Price</strong>: <input name="price" type="text" value="<?= $price ?>" required=""></p>
+            <p><strong>Stock</strong>: <br>
                 <input name="stock" type="text" size="40" value="<?= $stock ?>" required=""></p>
             <p><strong>Image</strong>: url (http:// or https://) or local file including path and file extension<br>
                 <input name="image" type="text" size="100" required value="<?= $image ?>"></p>
             <p><strong>Description</strong>:<br>
                 <textarea name="description" rows="8" cols="100"><?= $description ?></textarea></p>
-            <input type="submit" name="action" value="Update Movie">
+            <input type="submit" name="action" value="Update Vehicle">
             <input type="button" value="Cancel" onclick='window.location.href = "<?= BASE_URL . "/vehicle/detail/" . $id ?>"'>
         </form>
         <?php
