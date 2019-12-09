304430ace79a6bd64df020750a2bd836cb7e2534
jdivey <jdivey23@gmail.com>
2019-12-09 10:39:49 -0500

    New commit Date: 12-9-19 Time: 10:39 am.

---
+++ final_project/exceptions/data_missing_exception.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Author: Victor Gonzalez
+ * Date: 12/7/2019
+ * File: data_missing_exception.class.php
+ * Description:
+ */
+
+class DataMissingException extends Exception{
+    //There is no need of any code
+}
---
+++ final_project/views/login/login.class.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Author: Victor Gonzalez
+ * Date: 12/7/2019
+ * File: login.class.php
+ * Description:
+ */
+
+class Login extends IndexView
+{
+    public function display() {
+        ?>
+        <br>
+        <div class="top-row">Login</div>
+        <div class="middle-row">
+            <p>Please enter your username and password.</p>
+            <form method="post" action="../../index.php">
+                <div><input type="text" name="username" style="width: 99%" placeholder="Username"></div>
+                <div><input type="password" name="password" style="width: 99%" placeholder="Password"></div>
+                <div><input type="submit" class="button" value="Login"></div>
+            </form>
+        </div>
+        <div class="bottom-row">
+            <span style="float: left">Don't have an account? <a href="../../index.php">Register</a></span>
+            <span style="float: right"></span>
+        </div>
+
+
+
+        <?php
+
+    }
+
+
+}
